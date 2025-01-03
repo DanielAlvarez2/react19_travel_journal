6f673da051a435d192bf560e719d6e8d7fac858d
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-01-03 04:38:51 -0500

    section 2 lesson 2 Travel Journal Header Component

---
+++ fccSection2/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return(
+        <div className="main-header">
+            <h1>
+                <img src="./globe.png" alt="globe icon"></img>    
+                my travel journal
+            </h1>
+        </div>
+    )
+}
